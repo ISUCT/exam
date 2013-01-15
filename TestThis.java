656353c7c37c3b3b9f8c38d0f87d3662b79e0658
jskonst <jskonst@yandex.ru>
2013-01-15 18:11:48 +0400

    Файл с заданиями

---
+++ TestThis.java
@@ -0,0 +1,18 @@
+class TestThis{
+
+public int forTest(int a){
+   if (a<0){
+           return Math.abs(a);
+        }else{
+               if(a==0){
+                   return a;
+                   }
+               if(a<5){
+                   return a-1;
+                   }else{
+                      return a+1;
+                      }
+  } 
+        }
+
+}
